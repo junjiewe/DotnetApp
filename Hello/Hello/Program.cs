07ef15e89c9f20dfc9e4171efb39145a8096cdfb
Wei <610169@bah.com>
2020-03-11 09:24:06 -0400

    restart the project

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greeting());
+        }
+
+        public static String Greeting() {
+            return "Hello Testing";
+        } 
+    }
+}
---
+++ Hello/HelloTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8763e8a9-fa28-4ca3-9a6d-da4efec2d1ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
